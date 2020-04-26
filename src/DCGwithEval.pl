95c0848969d2f1c8521b832e751cfaf59ff27816
shalikar <60168233+shalikar@users.noreply.github.com>
2020-04-26 12:05:17 -0700

    fixed boolean comparison

--- src/DCGwithEval.pl
+++ src/DCGwithEval.pl
@@ -247,3 +247,266 @@ equal(Val1, Val2, false):- number(Val1), bool_keywords(Val2),
 
 equal(Val1, Val2, false):- string(Val1), bool_keywords(Val2), 
     writeln("String and boolean can not be compared."), fail.
+    
+equal(Val1, Val2, false):-  bool_keywords(Val1),number(Val2),
+    writeln("Boolean and Number can not be compared."), fail.
+
+equal(Val1, Val2, false):-  bool_keywords(Val1), string(Val2),
+    writeln("Boolean and String can not be compared."), fail.
+
+andCond(Val1,Val2,true):- Val1 = true, Val2 = true.
+andCond(Val1,Val2,false):- Val1 = false , Val2 = false.
+andCond(Val1,Val2,false):- Val1 = true , Val2 = false.
+andCond(Val1,Val2,false):- Val1 = false , Val2 = true.
+
+orCond(Val1,Val2, true):- Val1 = true, Val2 = true.
+orCond(Val1,Val2, true):- Val1 = true, Val2 = false.
+orCond(Val1,Val2, true):- Val1 = false, Val2 = true.
+orCond(Val1,Val2, false):- Val1 = false, Val2 = false.
+
+lesser(Val1,Val2,true):- Val1 < Val2.
+lesser(Val1,Val2,false):- Val1 >= Val2.
+greater(Val1,Val2,true):- Val1 > Val2.
+greater(Val1,Val2,false):- Val1 =< Val2.
+
+lesserEqual(Val1,Val2,true):- Val1 =< Val2.
+lesserEqual(Val1,Val2,false):- Val1 > Val2.
+greaterEqual(Val1,Val2,true):- Val1 >= Val2.
+greaterEqual(Val1,Val2,false):- Val1 < Val2.
+
+%--------------------------------------------------------------------------------
+:- table expr/3, term/3.
+
+assign(t_aBool(I,X)) --> word(I), [=], boolean(X).
+assign(t_aAssign(I,Y)) --> word(I),[=], assign(Y).
+assign(t_aInc(I)) --> word(I), [++].
+assign(t_aDec(I)) --> word(I), [--].
+assign(t_aAdd(I,X)) --> word(I), [+=], assign(X).
+assign(t_aSub(I,X)) --> word(I), [-=], assign(X).
+assign(t_aMult(I,X)) --> word(I), [*=], assign(X).
+assign(t_aDiv(I,X)) --> word(I), [/=], assign(X).
+assign(X) --> expr(X).
+
+expr(t_add(X,Y)) --> expr(X),[+],term(Y).
+expr(t_sub(X,Y)) --> expr(X),[-],term(Y).
+expr(X) --> term(X).
+
+term(t_mult(X,Y)) --> term(X),[*],paren(Y).
+term(t_div(X,Y)) --> term(X),[/],paren(Y).
+term(t_mod(X,Y)) --> term(X),['%'],paren(Y).
+term(X) --> paren(X).
+
+paren(t_paren(X)) --> ['('], assign(X), [')'].
+paren(X) --> number(X) | string_q(X) | word(X).
+
+% evaluate assignment statement
+booleanCheck(t_b(Y)):- Y = true; Y = false.
+    
+eval_expr(t_aBool(t_word(I),Y), Env, Val, NewEnv) :- 
+    eval_boolean(Y, Env,Env1, Val), update(I,Val,Env1,NewEnv).
+
+eval_expr(t_aAssign(t_word(I),Y), Env, Val, NewEnv) :-
+    \+ booleanCheck(t_b(Y)),
+    eval_expr(Y, Env,Val, Env1), update(I,Val,Env1,NewEnv).
+
+
+eval_expr(t_paren(X), Env, Val, NewEnv) :- eval_expr(X,Env, Val,NewEnv).
+
+eval_expr(t_aInc(t_word(I)), Env, Val, NewEnv) :-
+    eval_expr(t_add(t_word(I),t_num(1)), Env, Val, Env1), 
+    update(I,Val,Env1,NewEnv).
+
+eval_expr(t_aDec(t_word(I)), Env, Val, NewEnv) :-
+    eval_expr(t_sub(t_word(I),t_num(1)), Env, Val, Env1), 
+    update(I,Val,Env1,NewEnv).
+
+eval_expr(t_aAdd(t_word(I),Y), Env, Val, NewEnv) :-
+    eval_expr(t_add(t_word(I),Y), Env, Val, Env1),
+    update(I,Val,Env1,NewEnv).
+
+eval_expr(t_aSub(t_word(I),Y), Env, Val, NewEnv) :-
+    eval_expr(t_sub(t_word(I),Y), Env, Val, Env1), 
+    update(I,Val,Env1,NewEnv).
+
+eval_expr(t_aMult(t_word(I),Y), Env, Val, NewEnv) :-
+    eval_expr(t_mult(t_word(I),Y), Env, Val, Env1),
+    update(I,Val,Env1,NewEnv).
+
+eval_expr(t_aDiv(t_word(I),Y), Env, Val, NewEnv) :-
+    eval_expr(t_div(t_word(I),Y), Env, Val, Env1),
+    update(I,Val,Env1,NewEnv).
+
+% evaluate addition, subtraction, division, multiplication
+eval_expr(t_add(X,Y), Env, _Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1), 
+    eval_expr(Y, Env1, Val2, NewEnv), 
+    number(Val1), string(Val2),
+    writeln("int and string can not be concatenated"), fail.
+
+eval_expr(t_add(X,Y), Env, _Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1), 
+    eval_expr(Y, Env1, Val2, NewEnv), 
+    string(Val1), number(Val2),
+    writeln("string and int can not be concatenated"), fail.
+
+
+eval_expr(t_add(X,Y), Env, Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1), 
+    eval_expr(Y, Env1, Val2, NewEnv), 
+    \+string(Val1), \+string(Val2),
+    Val is Val1 + Val2.
+
+eval_expr(t_add(X,Y), Env, Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1), 
+    eval_expr(Y, Env1, Val2, NewEnv), 
+    \+ number(Val1), \+ number(Val2), 
+    atomic_concat(Val1, Val2, Val).
+
+eval_expr(t_sub(X,Y), Env, _Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    string(Val1), string(Val2),
+    writeln("string and string can not be subtrated"), fail.
+
+eval_expr(t_sub(X,Y), Env, _Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    string(Val1), number(Val2),
+    writeln("string and int can not be subtrated"), fail.
+
+eval_expr(t_sub(X,Y), Env, _Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    number(Val1), string(Val2),
+    writeln("int and string can not be subtrated"), fail.
+
+eval_expr(t_sub(X,Y), Env, Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    number(Val1), number(Val2),
+    Val is Val1 - Val2.
+
+eval_expr(t_mult(X,Y), Env, _Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    string(Val1), string(Val2),
+    writeln("string and string can not be multiplied"), fail.
+
+eval_expr(t_mult(X,Y), Env, Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    string(Val1), number(Val2),
+    multiply_string(Val1, Val2, Val).
+
+eval_expr(t_mult(X,Y), Env, Val, NewEnv):-
+	eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    number(Val1), string(Val2),
+    multiply_string(Val2, Val1, Val).
+
+eval_expr(t_mult(X,Y), Env, Val, NewEnv):-
+eval_expr(X, Env, Val1, Env1),  
+    eval_expr(Y, Env1, Val2, NewEnv), 
+    number(Val1), number(Val2),
+    Val is Val1 * Val2.
+
+eval_expr(t_div(X,Y), Env, _Val, NewEnv):-
+eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    string(Val1), string(Val2),
+    writeln("string and string can not be divided"), fail.
+
+eval_expr(t_div(X,Y), Env, _Val, NewEnv):-
+eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    string(Val1), number(Val2),
+    writeln("string and int can not be divided"), fail.
+
+eval_expr(t_div(X,Y), Env, _Val, NewEnv):-
+eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    number(Val1), string(Val2),
+    writeln("int and string can not be divided"), fail.
+
+eval_expr(t_div(X,Y), Env, Val, NewEnv):-
+eval_expr(X, Env, Val1, Env1), 
+    eval_expr(Y, Env1, Val2, NewEnv),
+    number(Val1), number(Val2),
+    Val is Val1 / Val2.
+
+eval_expr(t_mod(X,Y), Env, _Val, NewEnv):-
+eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    string(Val1), string(Val2),
+    writeln("modulus of string and int can not be evaluated"), fail.
+
+eval_expr(t_mod(X,Y), Env, _Val, NewEnv):-
+eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    string(Val1), number(Val2),
+    writeln("modulus of string and int can not be evaluated"), fail.
+
+eval_expr(t_mod(X,Y), Env, _Val, NewEnv):-
+eval_expr(X, Env, Val1, Env1),
+    eval_expr(Y, Env1, Val2, NewEnv),
+    number(Val1), string(Val2),
+    writeln("modulus of string and int can not be evaluated"), fail.
+
+eval_expr(t_mod(X,Y), Env, Val, NewEnv):-
+eval_expr(X, Env, Val1, Env1), 
+    eval_expr(Y, Env1, Val2, NewEnv), 
+    number(Val1), number(Val2),
+    Val is Val1 mod Val2.
+
+eval_expr(t_bool(I), Env, I, Env).
+eval_expr(t_word(I), Env, Val, Env):- lookup(I, Env, Val).
+eval_expr(t_num(X), Env, X, Env).
+eval_expr(t_string(X), Env, X, Env).
+
+% look for the variable value in the environment
+lookup(I, [(I,Val)|_], Val).
+lookup(I, [_|T], Val) :- lookup(I, T, Val).
+
+% update value of variable in the current environment
+update(Id, Val, [], [(Id, Val)]).
+update(Id, Val, [(Id,_)|T], [(Id,Val)|T]).
+update(Id, Val, [H|T], [H|R]) :-
+       H \= (Id,_), update(Id, Val,T,R).
+
+printseq(t_expr_print_ep(E,P))--> ['('],expr(E),[')'],[+], printseq(P).
+printseq(t_expr_print_pe(P,E))--> [P], {string(P)}, [+], ['('],expr(E),[')'].
+printseq(t_expr_print_pez(P,E,Z))--> [P], {string(P)}, [+], ['('],expr(E),[')'],[+], printseq(Z).
+printseq(t_expr_print_e(E)) --> 
+    \+printseq(t_expr_print_ep(_X,_Y)),
+    \+printseq(t_expr_print_pe(_Z,_T)),
+    expr(E).
+
+eval_printseq(t_expr_print_ep(E,Ps),Env,NewEnv,Val) :-
+    eval_expr(E,Env,Val1,Env1), eval_printseq(Ps,Env1,NewEnv,Val2),
+    atomic_concat(Val1, Val2, Val).
+
+eval_printseq(t_expr_print_pe(Ps,E),Env,NewEnv,Val) :-
+    eval_expr(E,Env,Val1,NewEnv),
+    atomic_concat(Ps, Val1, Val).
+
+eval_printseq(t_expr_print_pez(P,E,Z),Env,NewEnv,Val) :-
+   	eval_expr(E,Env,Val1,Env1), 
+    atomic_concat(P, Val1, Val2),
+    eval_printseq(Z,Env1,NewEnv,Val3),
+    atomic_concat(Val2, Val3, Val).
+
+eval_printseq(t_expr_print_e(E),Env,NewEnv,Val):- eval_expr(E,Env,Val,NewEnv).
+%-------------------------------------------------------------------------
+multiply_string(Val1, 1, Val1). 
+multiply_string(Val1, N, Val) :-  
+    N > 1, N1 is N - 1, multiply_string(Val1, N1, Val2),
+    atomic_concat(Val1, Val2, Val).
+multiply_string(_Val1, N, "") :-  
+    N =< 0.
+%--------------------------------------------------------------------------
+string_q(t_string(X)) --> [X],{ string(X)}.
+bool_keywords(true).
+bool_keywords(false).
+keywords([+,-,>,<,=,while,for,if,elif,else,print,true,false]).
+number(t_num(X)) --> [X],{number(X)}.
+word(t_word(X)) --> [X],{atom(X),keywords(K),\+member(X,K)}.
