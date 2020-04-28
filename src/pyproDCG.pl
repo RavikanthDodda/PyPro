8cd1a1a2c0eb65fa940535a4e02fd46aecf0f825
Somesh S <50901702+someshs301195@users.noreply.github.com>
2020-04-28 00:44:16 -0700

    mod assignment added

--- src/pyproDCG.pl
+++ src/pyproDCG.pl
@@ -155,8 +155,8 @@ assign(t_aAdd(I,X)) --> word(I), [+=], assign(X).
 assign(t_aSub(I,X)) --> word(I), [-=], assign(X).
 assign(t_aMult(I,X)) --> word(I), [*=], assign(X).
 assign(t_aDiv(I,X)) --> word(I), [/=], assign(X).
-assign(t_aDiv(I,X)) --> word(I), [/=], assign(X).
 assign(t_aIDiv(I,X)) --> word(I), [//=], assign(X).
+assign(t_aMod(X,Y)) --> word(X),['%'],[=],assign(Y).
 assign(X) --> expr(X).
 
 expr(t_add(X,Y)) --> expr(X),[+],term(Y).
