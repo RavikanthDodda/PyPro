1ddedb13d10884515490537a78c4393fa5da53c1
Ravikanth Reddy <ravikantreddy97@gmail.com>
2020-04-28 18:18:42 -0700

    syntax and runtime errors distinguished.

--- src/pypro.py
+++ src/pypro.py
@@ -5,14 +5,14 @@ from modules.lexer import Lexer
 # Checking file extension
 file_ext = sys.argv[1][-3:]
 if file_ext == ".pr":
-# Handling file operations and file errors
+    # Handling file operations and file errors
     try:
         with open(sys.argv[1], "r") as inp_file:
             data = inp_file.read()
-# Getting lexer
+            # Getting lexer
             lexer = Lexer()
             tokenlist = "["
-# Generating tokenlist          
+            # Generating tokenlist
             for tok in lexer.tokenize(data):
                 tokenlist += tok.value + ","
 
@@ -20,17 +20,24 @@ if file_ext == ".pr":
             tokenlist += "]"
             if tokenlist == "]":
                 tokenlist = "[]"
-# Interfacing with prolog
+            # Interfacing with prolog
             prolog = Prolog()
-            prolog.consult('./modules/pyproDCG.pl')
-            prolog.consult('./modules/pyproEval.pl')
+            prolog.consult('modules/pyproDCG.pl')
+            prolog.consult('modules/pyproEval.pl')
             if not lexer.err:
-# Querying for parse tree and evaluation by passing tokenlist
-                t = list(prolog.query(
-                    "program(P,"+tokenlist+", []),eval_program(P)"))
-# Checking for runtime errors
-                if not bool(t) and tokenlist != "[]":
-                    print("Runtime Error: Please check the code")
+                # Querying for parse tree by passing tokenslist
+                ast = list(prolog.query(
+                    "program(P,"+tokenlist+", [])"))
+                # Checking for syntax errors
+                if not bool(ast) and tokenlist != "[]":
+                    print("Syntax Error: Please check the code")
+                else:
+                    # Querying for semantics and runtime by passing parsetree
+                    result = list(prolog.query(
+                    "eval_program("+ ast[0]['P']+")"))
+                    # Checking for runtime errors
+                    if not bool(result):
+                        print("Runtime Error: Please check the code")
     except FileNotFoundError:
         print("No such file in path:", sys.argv[1])
 else:
